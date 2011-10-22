3df7e0f1c8c1882d6ee7a2779a0b92dcd6125c5c
Luigi Tullio <tluigi@gmail.com>
2011-10-22 23:05:10 +0200

    Add lincense in some files.

--- src/monthdelta.py
+++ src/monthdelta.py
@@ -1,20 +1,5 @@
 # -*- coding: UTF-8 -*-
 
-#
-# NTM Copyright (C) 2009-2011 by Luigi Tullio <tluigi@gmail.com>.
-#
-#   NTM is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free
-# Software Foundation; either version 2 of the License, or (at your option)
-# any later version.
-#
-#   NTM is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-
-
 """monthdelta
 
 Date calculation with months: monthdelta class and monthmod() function.
--- src/run_ntm.sh
+++ src/run_ntm.sh
@@ -14,6 +14,7 @@
 # GNU General Public License for more details.
 #
 
+
 logfile=~/.ntm/ntm.log
 ntmdd=~/.ntm
 
